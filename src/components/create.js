d0d8b79135fb22f94c26da32fe4d304ca2396556
NickFeige <nfeige7@gmail.com>
2023-10-20 09:25:25 +0000

    added adding button

--- src/components/create.js
+++ src/components/create.js
@@ -6,11 +6,15 @@ function Create() {
     const [author, setAuthor] = useState('');
     const [url, setUrl] = useState('');
     //my forms to add info
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log("Title: "+title+"Author: "+author+"Url Cover: "+url);
+    }
     return (
         <div>
             <h3>hello from create</h3>
-            
-            <form>
+           
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label>Add Book Title: </label>
                     <input type="text"
@@ -23,16 +27,16 @@ function Create() {
                     <label>Add Book Aurthor: </label>
                     <input type="text"
                         className="form-control"
-                        value={title}
-                        onChange={(e) => { setTitle(e.target.value) }}
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}
                     />
                 </div>
                 <div className="form-group">
                     <label>Add front page url: </label>
                     <input type="text"
                         className="form-control"
-                        value={title}
-                        onChange={(e) => { setTitle(e.target.value) }}
+                        value={url}
+                        onChange={(e) => { setUrl(e.target.value) }}
                     />
                 </div>
                 <div>
