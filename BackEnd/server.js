0d930e42b5afec4de2c82246845984548b5ff57d
NickFeige <nfeige7@gmail.com>
2023-12-08 09:43:29 +0000

    lab10

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
 const cors = require('cors');
 app.use(cors());
 app.use(function(req, res, next) {
@@ -73,6 +77,10 @@ app.get('/api/book',async(req,res)=>{
     let book = await bookModel.findById({_id:req.params.id})
     res.send(book);
   })
+  app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
+    });
+    
  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
